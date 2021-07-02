593c9da4d9c59b5c6de83a90463c1d9f4387fa67
Vincent Yuan <vincentyuan1218@126.com>
2021-07-02 18:21:15 +0800

    Add comments

--- docs/Scripts/Update_reading_time.py
+++ docs/Scripts/Update_reading_time.py
@@ -1,9 +1,12 @@
+# Dependencies
 import os
 import re
 from datetime import datetime
 import numpy as np
 import markdown
 
+
+# Post list
 Folder_list = ['../Dessert','../Main_Course']
 
 Post_list = []
@@ -14,16 +17,20 @@ for folder in Folder_list:
             print(os.path.join(root, name))
             if '.md' in name:
                 Post_list.append(os.path.join(root, name))
-                
+
+
+# Update the reading time
 for post in Post_list:
     f = open(post, 'r')
-    htmlmarkdown="".join(f.readlines())
+    markdown="".join(f.readlines())
+
     match = re.search(r'\d{4}-\d{2}-\d{2}', post.split('/')[-1])
     date = datetime.strptime(match.group(), '%Y-%m-%d').date()
-    htmlmarkdown = re.sub(r"发布于\d+-\d+-\d+，阅读时间", "发布于{}，阅读时间".format(date), htmlmarkdown)
-    chinese_word = re.findall(r'[\u4E00-\u9FFF]',htmlmarkdown)
-    print(len(chinese_word))
-    htmlmarkdown = re.sub(r"阅读时间：约\d+分钟", "阅读时间：约{}分钟".format(int(len(chinese_word)/200)), htmlmarkdown)
-    
-    with open(post, 'w') as new_file:    
-        new_file.write(htmlmarkdown)
+
+    markdown = re.sub(r"发布于\d+-\d+-\d+，阅读时间", "发布于{}，阅读时间".format(date), markdown)
+
+    chinese_word = re.findall(r'[\u4E00-\u9FFF]',markdown)
+    markdown = re.sub(r"阅读时间：约\d+分钟", "阅读时间：约{}分钟".format(int(len(chinese_word)/200)), markdown)
+
+    with open(post, 'w') as new_file:
+        new_file.write(markdown)
